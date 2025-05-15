de4e094483aca32f13df475b387228da8a49d21b
ayyooba <ayyooba@fortude.co>
2025-05-15 09:57:15 +0530

    Login endpoint added

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello from server....')
 })
 
+app.get('/login', (req, res) => {
+    res.send('Login....')
+})
+
 app.listen(PORT, (req, res) => {
     console.log(`Application is running on port ${PORT}`)
 })
