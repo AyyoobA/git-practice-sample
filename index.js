f5044cad650e807e1625193ab37d529776f340e3
ayyooba <ayyooba@fortude.co>
2025-05-15 12:02:33 +0530

    Removed: Comment for user registration

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post('/register', express.json(), (req, res) => {
     if (!username || !password) {
         return res.status(400).send('Username and password are required');
     }
-    // In a real app, you'd save the user to a database here
+    
     res.status(201).send('Registration successful');
 });
 
